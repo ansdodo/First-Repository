8562e10831ed3a24f4a2f673307ca9702c8d7e8d
ansdodo <mjh608@naver.com>
2021-12-08 13:23:26 +0900

    first commit

--- src/util/Gmail.java
+++ src/util/Gmail.java
@@ -6,6 +6,6 @@ import javax.mail.Authenticator;
 public class Gmail extends Authenticator{
 	
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("ansmomo0o@gmail.com", "fkdtk!59");
+		return new PasswordAuthentication("ansmomo0o@gmail.com", "구글 비밀번호");
 	}
 }
